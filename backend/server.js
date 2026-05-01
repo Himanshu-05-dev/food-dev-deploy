96184d277a24ff2229b1231a48c3185a59e8c8fd
Himanshu Gola <himanshu.gola05@gmail.com>
2026-05-01 14:39:06 +0530

    changes for backend

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,8 @@ import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 
-
-
 // app config
 const app = express()
-// const port = 4000
 
 // middleware
 app.use(express.json())
@@ -20,21 +17,15 @@ app.use(cors())
 // DB Connections
 connectDB();
 
-// API endpoint 
-app.use("/api/food",foodRouter)
-app.use("/images",express.static("uploads"))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+// API endpoints
+app.use("/api/food", foodRouter)
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("API Working")
 })
 
-// app.listen(port,()=>{
-//     console.log(`Server started on http://localhost:${port}`)
-// })
-
-
-// mongodb+srv://foodproject:99887766@cluster0.q7ws6oe.mongodb.net/?
+// Export app for Vercel serverless
+export default app
